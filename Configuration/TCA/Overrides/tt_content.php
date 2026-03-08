86239cf9dccaea449897eb417fcb58cefc4cd684
maispace <joel@maispace.de>
2026-03-08 17:17:09 +0100

    [FEATURE] Implement new content elements for TYPO3 extension

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,2 +1,48 @@
 <?php
 
+(new \Maispace\Base\RegistrationHelper\CType('element-headline', 'Überschrift', 'content-header'))
+    ->addDefaultHeaderPalette()
+    ->register();
+
+(new \Maispace\Base\RegistrationHelper\CType('element-text', 'Text', 'content-text'))
+    ->addCustomFields('bodytext')
+    ->addColumnOverride('bodytext', [
+        'config' => [
+            'enableRichtext' => 1
+        ],
+    ])
+    ->register();
+
+(new \Maispace\Base\RegistrationHelper\CType('element-html', 'Html', 'content-special-html'))
+    ->addCustomFields('bodytext')
+    ->addColumnOverride('bodytext', [
+        'config' => [
+            'format' => 'html',
+            'renderType' => 'codeEditor',
+            'wrap' => 'off'
+        ],
+    ])
+    ->register();
+
+(new \Maispace\Base\RegistrationHelper\CType('element-table', 'Tabelle', 'content-table'))
+    ->addCustomFields('bodytext')
+    ->addColumnOverride('bodytext', [
+        'config' => [
+            'renderType' => 'textTable',
+            'wrap' => 'off'
+        ],
+    ])
+    ->register();
+
+
+(new \Maispace\Base\RegistrationHelper\CType('element-image', 'Bild', 'content-image'))
+    ->addCustomFields('image')
+    ->register();
+
+(new \Maispace\Base\RegistrationHelper\CType('element-video', 'Video', 'content-media'))
+    ->addCustomFields('media')
+    ->register();
+
+(new \Maispace\Base\RegistrationHelper\CType('element-file', 'Dokument', 'module-documentation'))
+    ->addCustomFields('media')
+    ->register();
